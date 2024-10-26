0feff6733e6bbc50bac9f323f51af62eadd24a5a
aderemi amos <73750355+gabrieladeremi@users.noreply.github.com>
2024-10-26 15:42:30 +0100

    create todo implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 import TodoList from './components/TodoList';
-import { get_todos } from './api/endpoints';
+import AddTodo from './components/AddTodo';
+
+import { get_todos, create_todo } from './api/endpoints';
 
 import { useState, useEffect } from 'react';
 
@@ -22,10 +23,17 @@ function App() {
     fetchTodos();
   }, [])
 
+  const addTodo = async (todo_name) => {
+    const todo = create_todo(todo_name)
+
+    setTodos([todo, ...todos])
+  }
+
   return (
     <div className='App'>
       <div className='app-container'>
         <h1 className='title'>Todo App</h1>
+        <AddTodo addTodo={addTodo} />
         <TodoList todos={todos}/>
       </div>
     </div>
